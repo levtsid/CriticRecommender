58296564c12dca6adc956c7959ca475dc85943fb
levtsid <levtsid@brandeis.edu>
2023-04-20 12:38:17 -0400

    Update critictf.py
    updated comments

--- critictf.py
+++ critictf.py
@@ -30,7 +30,7 @@ newcheck(bigset,rev)
 revdf = pd.read_csv("rev.csv") #,sep="\\t")
 bigdf = pd.read_csv("big.csv") #,sep="\\t")
 
-#keep only reviewers with 10+ reviews
+#keep only reviewers with 10+ reviews, and movies with 10+ reviews
 revdf2 = revdf.dropna()
 names = revdf2.value_counts(subset = ['name'])
 revdfnames = revdf[revdf['name'].isin(names[names>9].reset_index().name)]
@@ -45,6 +45,8 @@ revdd= revdfrevs.drop(columns = ['Unnamed: 0','linkr'])
 for i in range(len(revdd['rate'].to_list())):
     spar[revdd['revi'].to_list()[i]][revdd['name'].to_list()[i]] = revdd['rate'].to_list()[i]
     
+    
+#convert dataframe to dataset of tensor slices
 spf = spar.astype(pd.SparseDtype("float", np.nan))
 
 revspar  = tf.data.Dataset.from_tensor_slices(spf)
