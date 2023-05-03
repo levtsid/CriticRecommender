27c6c90b7e0785c4af63939c879e1df0e6fe67b4
levtsid <levtsid@brandeis.edu>
2023-05-03 17:30:06 -0400

    rearrange and remove excess

--- critictf.py
+++ critictf.py
@@ -35,19 +35,17 @@ revdf2 = revdf.dropna()
 
 names = revdf2.value_counts(subset = ['name'])
 revdfnames = revdf[revdf['name'].isin(names[names>9].reset_index().name)]
+namemin = names[names>9].reset_index().name
 
 revs = revdfnames.value_counts(subset = ['revi'])
 revdfrevs = revdfnames[revdfnames['revi'].isin(revs[revs>9].reset_index().revi)]
+revmin = revs[revs>9].reset_index().revi
 
 revdd= revdfrevs.drop(columns = ['Unnamed: 0','linkr'])
 
-revmin = revs[revs>9].reset_index().revi
-namemin = names[names>9].reset_index().name
-
-
 #ignore this bit, it's very ugly and needs to be fixed later
-for i in range(len(revdd['rate'].to_list())):
-    spar[revdd['revi'].to_list()[i]][revdd['name'].to_list()[i]] = revdd['rate'].to_list()[i]
+#for i in range(len(revdd['rate'].to_list())):
+#    spar[revdd['revi'].to_list()[i]][revdd['name'].to_list()[i]] = revdd['rate'].to_list()[i]
     
 #replacement code for ugly loop
 index=list(revmin)
